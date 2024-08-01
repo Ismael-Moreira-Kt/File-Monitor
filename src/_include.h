e42f65f2c52cf2a6791742f731b011956c6d5aca
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-08-01 11:02:44 +0100

    feat: Addition of the file that imports all the libraries needed for the project.

---
+++ src/_include.h
@@ -0,0 +1,17 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+
+
+#include <stddef.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <gtk/gtk.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+
+
+#endif
